31d7f3613aabfba6650c2d97781f0d31d3a69b26
Deivid Momchilov <deividmom2006@gmail.com>
2026-04-09 11:18:47 +0300

    Add ToDoApp API project and config

---
+++ api/ToDoApp/Common/EntityValidation.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoApp.Common
+{
+    public class EntityValidation
+    {
+        public const int TitleMinLength = 3;
+        public const int TitleMaxLength = 100;
+
+        public const int PositionMinValue = 0;
+        public const int PositionMaxValue = 100;
+    }
+}
---
+++ api/ToDoApp/Models/ToDoItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static ToDoApp.Common.EntityValidation;
+
+namespace ToDoApp.Models
+{
+    public class ToDoItem
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public bool IsCompleted { get; set; }
+
+        [Required]
+        public int Position { get; set; }
+    }
+}
